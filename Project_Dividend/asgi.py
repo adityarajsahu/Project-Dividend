6de119e83820dcd0f632097c479986d698e4e37c
Adityaraj Sahu <adityaraj2019.sahu@gmail.com>
2022-12-28 13:23:27 +0530

    new django project created

---
+++ Project_Dividend/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Project_Dividend project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project_Dividend.settings')
+
+application = get_asgi_application()
